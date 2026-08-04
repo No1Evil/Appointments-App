b36745d61ce76d7432352b1b4bda9be21bff22cb
No1Evil <notevil036@gmail.com>
2026-08-04 16:13:48 +0300

    feat(backend): implement AppointmentFactory

---
+++ appointments-service/src/main/java/com/tsumakov/appointments/appointment/AppointmentFactory.java
@@ -0,0 +1,45 @@
+package dev.tsumakov.appointments.appointment;
+
+import dev.tsumakov.appointments.appointment.status.AppointmentStatus;
+import dev.tsumakov.appointments.common.AppointmentObjects;
+import dev.tsumakov.appointments.common.factory.UuidFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AppointmentFactory {
+
+    private final UuidFactory uuidFactory;
+
+    public Appointment create(
+        UUID slotId, UUID patientId, String serviceName, UUID practitionerId,
+        OffsetDateTime startTime, OffsetDateTime endTime, AppointmentStatus status
+    ) {
+        Objects.requireNonNull(slotId, "Slot id must not be null");
+        Objects.requireNonNull(patientId, "Patient id must not be null");
+        AppointmentObjects.requireNotBlank(serviceName, "Service name");
+        AppointmentObjects.requireValidDates(startTime, endTime);
+        Objects.requireNonNull(status, "Status cannot be null");
+
+        UUID appointmentId = uuidFactory.generate();
+        OffsetDateTime now = OffsetDateTime.now();
+
+        return Appointment.builder()
+            .id(appointmentId)
+            .slotId(slotId)
+            .patientId(patientId)
+            .serviceName(serviceName)
+            .practitionerId(practitionerId)
+            .startTime(startTime)
+            .endTime(endTime)
+            .status(status)
+            .createdAt(now)
+            .updatedAt(now)
+            .build();
+    }
+}
