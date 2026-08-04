c7da0f9d8a3c79c1c494dfe54de22fa46f21df3e
No1Evil <notevil036@gmail.com>
2026-08-04 16:13:48 +0300

    feat(backend): add CrudRepository interface

---
+++ appointments-service/src/main/java/com/tsumakov/appointments/common/repository/CrudRepository.java
@@ -0,0 +1,20 @@
+package dev.tsumakov.appointments.common.repository;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<E, ID> {
+
+    Optional<E> findBy(@Nonnull ID identifier) throws DataAccessException;
+
+    List<E> findAll() throws DataAccessException;
+
+    ID create(@Nonnull E entity) throws DataAccessException;
+
+    E update(@Nonnull E entity) throws DataAccessException;
+
+    boolean delete(@Nonnull ID identifier) throws DataAccessException;
+}
