e4437845118df3cb7b6ba9faae424613f347f6e0
No1Evil <notevil036@gmail.com>
2026-08-04 21:26:37 +0300

    refactor(backend): refactor ServiceCategoryService

--- appointments-service/src/main/java/com/tsumakov/appointments/service/ServiceCategoryService.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/service/ServiceCategoryService.java
@@ -1,14 +1,28 @@
 package dev.tsumakov.appointments.service;
 
+import dev.tsumakov.appointments.service.exception.ServiceCategoryNotFound;
 import jakarta.inject.Singleton;
 import java.util.List;
+import java.util.Map;
 
 @Singleton
 public class ServiceCategoryService {
 
+  private static final Map<String, ServiceCategory> serviceCategories = Map.of(
+      "dental", new ServiceCategory().setCode("dental").setName("Dental Service"),
+      "gp", new ServiceCategory().setCode("gp").setName("General Practitioner"),
+      "mental-health", new ServiceCategory().setCode("mental-health").setName("Mental Health")
+  );
+
   public List<ServiceCategory> getServices() {
-    return List.of(new ServiceCategory().setCode("dental").setName("Dental Service"),
-        new ServiceCategory().setCode("gp").setName("General Practitioner"),
-        new ServiceCategory().setCode("mental-health").setName("Mental Health"));
+    return serviceCategories.values().stream().toList();
+  }
+
+  public ServiceCategory getServiceByCode(String code) {
+    var service = serviceCategories.get(code);
+    if (service == null) {
+      throw new ServiceCategoryNotFound("No such service category with code " + code);
+    }
+    return service;
   }
 }
