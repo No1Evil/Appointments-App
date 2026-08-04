f078f9f49e8c836920068f4920f6856397b3e44c
No1Evil <notevil036@gmail.com>
2026-08-04 16:13:48 +0300

    feat(backend): implement Appointment

---
+++ appointments-service/src/main/java/com/tsumakov/appointments/appointment/Appointment.java
@@ -0,0 +1,49 @@
+package dev.tsumakov.appointments.appointment;
+
+import dev.tsumakov.appointments.appointment.exception.AppointmentAlreadyCancelled;
+import dev.tsumakov.appointments.appointment.exception.CannotCancelCompletedAppointment;
+import dev.tsumakov.appointments.appointment.status.AppointmentStatus;
+import dev.tsumakov.appointments.common.AppointmentObjects;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Builder(access = AccessLevel.PACKAGE)
+@AllArgsConstructor
+public class Appointment {
+    private UUID id;
+    private UUID slotId;
+    private UUID patientId;
+
+    private final String serviceName; // name of the service from the slot selected
+    private final UUID practitionerId;
+    private OffsetDateTime startTime;
+    private OffsetDateTime endTime;
+    private String comment;
+
+    private AppointmentStatus status;
+
+    private final OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+    public void cancel() {
+        if (this.status == AppointmentStatus.CANCELLED) {
+            throw new AppointmentAlreadyCancelled("Appointment is already cancelled");
+        } else if (this.status == AppointmentStatus.COMPLETED) {
+            throw new CannotCancelCompletedAppointment("Cannot cancel a completed appointment");
+        }
+        this.status = AppointmentStatus.CANCELLED;
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    public void updateDetails(OffsetDateTime newStart, OffsetDateTime newEnd, String newComment) {
+        AppointmentObjects.requireValidDates(newStart, newEnd);
+        Objects.requireNonNull(newComment);
+        this.startTime = newStart;
+        this.endTime = newEnd;
+        this.comment = newComment;
+    }
+}
