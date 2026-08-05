e7e54d9e7e3520ea33e0502f63ba4a448021eb2d
No1Evil <notevil036@gmail.com>
2026-08-05 13:19:24 +0300

    feat(backend): apply ToString, EqualsAndHashCode annotations to Patient entity

--- appointments-service/src/main/java/com/tsumakov/appointments/patient/Patient.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/patient/Patient.java
@@ -5,13 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 import java.util.UUID;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @Builder(access = AccessLevel.PACKAGE)
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Patient {
 
+  @EqualsAndHashCode.Include
   private UUID id;
   private String firstName;
   private String lastName;
