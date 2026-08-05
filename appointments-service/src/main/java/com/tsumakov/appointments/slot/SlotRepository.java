698708cd2d68fc3bf1ea6b4612387af4b8937cdf
No1Evil <notevil036@gmail.com>
2026-08-05 22:07:12 +0300

    refactor(backend): refactor Slot, SlotRepository, SlotService

--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotRepository.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotRepository.java
@@ -1,50 +1,93 @@
 package dev.tsumakov.appointments.slot;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import dev.tsumakov.appointments.common.repository.CrudRepository;
+import dev.tsumakov.appointments.service.ServiceCategoryService;
+import dev.tsumakov.appointments.slot.status.SlotStatus;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class SlotRepository {
+public class SlotRepository implements CrudRepository<Slot, Long> {
+
   private final JdbcTemplate jdbcTemplate;
+  private final ServiceCategoryService serviceCategoryService;
 
-  public Slot load(Long id) {
-    String sql = "select * from slot where id = ?";
+  private final RowMapper<Slot> rowMapper = ((rs, rowNum) -> Slot.builder()
+      .id(rs.getLong("id"))
+      .status(SlotStatus.valueOf(rs.getString("status")))
+      .service(serviceCategoryService.getServiceByCode(rs.getString("service")))
+      .startTime(rs.getObject("start_time", OffsetDateTime.class))
+      .endTime(rs.getObject("end_time", OffsetDateTime.class))
+      .build());
 
-    return jdbcTemplate.queryForObject(sql, new SlotRowMapper(), id);
+  @Override
+  public Optional<Slot> findBy(@NonNull Long identifier) throws DataAccessException {
+    String sql = "select * from slots where id = ?";
+    var query = jdbcTemplate.query(sql, rowMapper, identifier);
+    return query.stream().findFirst();
   }
 
-  public List<Slot> getSlots(SlotParams params) {
+  @Override
+  public List<Slot> findAll() throws DataAccessException {
+    String sql = "select * from slots";
+    return jdbcTemplate.query(sql, rowMapper);
+  }
+
+  @Override
+  public Long create(@NonNull Slot entity) throws DataAccessException {
+    String sql = """
+        insert into slots (
+          status,
+          service,
+          start_time,
+          end_time
+        ) values (?, ?, ?, ?, ?)
+        """;
+    int rowsUpdated = jdbcTemplate.update(sql, entity.getStatus().toString(),
+        entity.getService().getCode(), entity.getStartTime(), entity.getEndTime());
+    return 0L;
+  }
+
+  @Override
+  public boolean update(@NonNull Slot entity) throws DataAccessException {
+    String sql = """
+        update slots set
+          status = ?,
+          service = ?,
+          start_time = ?,
+          end_time = ?
+        where id = ?
+        """;
+    int rowsUpdated = jdbcTemplate.update(sql, entity.getStatus().toString(),
+        entity.getService().getCode(), entity.getStartTime(), entity.getEndTime(), entity.getId());
+    return rowsUpdated > 0;
+  }
+
+  @Override
+  public boolean delete(@NonNull Long identifier) throws DataAccessException {
+    String sql = "delete from slots where id = ?";
+    int rowsUpdated = jdbcTemplate.update(sql, identifier);
+    return rowsUpdated > 0;
+  }
+
+  public List<Slot> listByFilter(SlotParams params) {
     String sql = "select * from slot";
     sql += buildFilters(params);
-
-    return jdbcTemplate.query(sql, new SlotRowMapper());
+    return jdbcTemplate.query(sql, rowMapper);
   }
 
   private String buildFilters(SlotParams params) {
     //TODO: build filters here
 
     return "";
-  }
-
-  private static class SlotRowMapper implements RowMapper<Slot> {
-    @Override
-    public Slot mapRow(ResultSet rs, int rowNum) throws SQLException {
-      Slot slot = new Slot();
-      slot.setId(rs.getLong("id"));
-      slot.setStatus(rs.getString("status"));
-      slot.setService(rs.getString("service"));
-      slot.setStartTime(rs.getObject("start_time", OffsetDateTime.class));
-      slot.setEndTime(rs.getObject("end_time", OffsetDateTime.class));
-
-      return slot;
-    }
   }
 
 }
--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotService.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotService.java
@@ -1,21 +1,58 @@
 package dev.tsumakov.appointments.slot;
 
-import jakarta.inject.Singleton;
+import dev.tsumakov.appointments.slot.exception.SlotIsTakenException;
+import dev.tsumakov.appointments.slot.exception.SlotNotFoundException;
+import jakarta.annotation.Nonnull;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Singleton
+@Service
 @RequiredArgsConstructor
 public class SlotService {
+
   private final SlotRepository slotRepository;
 
   public Slot getSlot(Long id) {
-    return slotRepository.load(id);
+    return slotRepository.findBy(id)
+        .orElseThrow(() -> new SlotNotFoundException("Slot with id " + id + " not found"));
+  }
+
+  public Slot getValidSlotForAppointment(@Nonnull Long id) {
+    Slot slot = getSlot(id);
+
+    slot.validateNotExpired();
+
+    if (!slot.isFree()) {
+      throw new SlotIsTakenException("Slot is not available");
+    }
+
+    return slot;
   }
 
   public List<Slot> getSlots(SlotParams params) {
-    return slotRepository.getSlots(params);
+    return slotRepository.listByFilter(params);
   }
 
-  //TODO: add some methods to operate with slots
+  @Transactional
+  public void markSlotScheduled(@Nonnull Long slotId) {
+    Slot slot = getSlot(slotId);
+    slot.markAsScheduled();
+    slotRepository.update(slot);
+  }
+
+  @Transactional
+  public void markSlotBooked(@Nonnull Long slotId) {
+    Slot slot = getSlot(slotId);
+    slot.markAsBooked();
+    slotRepository.update(slot);
+  }
+
+  @Transactional
+  public void markSlotBooked(@Nonnull Slot slot) {
+    slot.markAsBooked();
+    slotRepository.update(slot);
+  }
 }
