d9fb0b261d275a130bdac5d2a374d0402fc502cb
No1Evil <notevil036@gmail.com>
2026-08-05 13:19:37 +0300

    feat(backend): apply ToString, EqualsAndHashCode annotations to Practitioner entity

--- appointments-service/src/main/java/com/tsumakov/appointments/practitioner/Practitioner.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/practitioner/Practitioner.java
@@ -1,10 +1,19 @@
 package dev.tsumakov.appointments.practitioner;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.util.UUID;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-@Data
+@Getter
+@ToString
+@Builder(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Practitioner {
     private UUID id;
     private String firstName;
