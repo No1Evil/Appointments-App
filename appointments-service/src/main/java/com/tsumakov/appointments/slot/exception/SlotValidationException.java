157162ecf18039b6f56da8de7483d5bd019b1548
No1Evil <notevil036@gmail.com>
2026-08-05 22:06:46 +0300

    feat(backend): add SlotValidationException

---
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/exception/SlotValidationException.java
@@ -0,0 +1,8 @@
+package dev.tsumakov.appointments.slot.exception;
+
+public class SlotValidationException extends RuntimeException {
+
+  public SlotValidationException(String message) {
+    super(message);
+  }
+}
