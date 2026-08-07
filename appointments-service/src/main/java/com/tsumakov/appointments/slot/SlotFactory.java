85495c0880e50c2855a4d94327b4c71575490a66
No1Evil <notevil036@gmail.com>
2026-08-07 20:41:30 +0300

    feat(backend): SlotFactory add params validation

--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotFactory.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotFactory.java
@@ -1,9 +1,11 @@
 package dev.tsumakov.appointments.slot;
 
+import dev.tsumakov.appointments.common.AppointmentObjects;
 import dev.tsumakov.appointments.service.ServiceCategory;
 import dev.tsumakov.appointments.slot.status.SlotStatus;
 import jakarta.inject.Singleton;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 
 @Singleton
@@ -12,6 +14,10 @@ public class SlotFactory {
 
   public Slot create(SlotStatus status, ServiceCategory service,
       OffsetDateTime startTime, OffsetDateTime endTime) {
+    Objects.requireNonNull(status, "Status cannot be null");
+    Objects.requireNonNull(service, "Service cannot be null");
+    AppointmentObjects.requireValidDates(startTime, endTime);
+
     return Slot.builder()
         .id(null)
         .status(status)
