e06e5a3ca174a39e2c1e888e4192349802a12709
No1Evil <notevil036@gmail.com>
2026-08-07 15:18:43 +0300

    refactor(backend): refactor SlotController

--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotController.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotController.java
@@ -1,24 +1,32 @@
 package dev.tsumakov.appointments.slot;
 
+import dev.tsumakov.appointments.slot.mapper.SlotMapper;
+import dev.tsumakov.appointments.slot.web.response.SlotResponse;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.annotation.Nullable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
-@Controller("slots")
+@Controller("/slots")
 @RequiredArgsConstructor
 public class SlotController {
+
   private final SlotService slotService;
+  private final SlotMapper slotMapper;
 
   @Get("/{id}")
-  public Slot getSlot(Long id) {
-    return slotService.getSlot(id);
+  public HttpResponse<SlotResponse> get(Long id) {
+    var result = slotService.getSlot(id);
+    return HttpResponse.ok(slotMapper.toResponse(result));
   }
 
   @Get("{?params*}")
-  public List<Slot> getSlots(@Nullable SlotParams params) {
-    return slotService.getSlots(params);
+  public HttpResponse<List<SlotResponse>> getSlots(@Nullable SlotParams params) {
+    var result = slotService.getSlots(params);
+    var response = result.stream().map(slotMapper::toResponse).toList();
+    return HttpResponse.ok(response);
   }
 
 }
