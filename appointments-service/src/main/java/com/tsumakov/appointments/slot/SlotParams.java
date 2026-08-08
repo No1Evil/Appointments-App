a99b9f7ecd583215cccbc74b4a41543b60073f47
No1Evil <notevil036@gmail.com>
2026-08-08 19:06:10 +0300

    fix(backend): mark SlotParams Introspected and add format OffsetDateTime from String

--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotParams.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotParams.java
@@ -1,13 +1,16 @@
 package dev.tsumakov.appointments.slot;
 
 import dev.tsumakov.appointments.slot.status.SlotStatus;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.convert.format.Format;
 import jakarta.annotation.Nullable;
 import java.time.OffsetDateTime;
 
+@Introspected
 public record SlotParams(
     @Nullable SlotStatus status,
     @Nullable String serviceCode,
-    @Nullable OffsetDateTime startTime
+    @Nullable @Format("yyyy-MM-dd'T'HH:mm:ss.SSSX") OffsetDateTime startTime
 ) {
 
 }
