f024368fef26a119c60de31fad78bd9f33c6a9d1
No1Evil <notevil036@gmail.com>
2026-08-09 11:13:59 +0300

    feat(backend): add DateValidationExceptionHandler, StringValidationExceptionHandler

---
+++ appointments-service/src/main/java/com/tsumakov/appointments/common/web/config/DateValidationExceptionHandler.java
@@ -0,0 +1,21 @@
+package dev.tsumakov.appointments.common.web.config;
+
+import dev.tsumakov.appointments.common.exception.DateValidationException;
+import dev.tsumakov.appointments.common.web.ErrorResponse;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import jakarta.inject.Singleton;
+
+@Produces
+@Singleton
+public final class DateValidationExceptionHandler
+    implements ExceptionHandler<DateValidationException, HttpResponse<?>> {
+
+  @Override
+  public HttpResponse<?> handle(HttpRequest request, DateValidationException exception) {
+    return HttpResponse.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+  }
+}
---
+++ appointments-service/src/main/java/com/tsumakov/appointments/common/web/config/StringValidationExceptionHandler.java
@@ -0,0 +1,21 @@
+package dev.tsumakov.appointments.common.web.config;
+
+import dev.tsumakov.appointments.common.exception.StringValidationException;
+import dev.tsumakov.appointments.common.web.ErrorResponse;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import jakarta.inject.Singleton;
+
+@Produces
+@Singleton
+public final class StringValidationExceptionHandler
+    implements ExceptionHandler<StringValidationException, HttpResponse<?>> {
+
+  @Override
+  public HttpResponse<?> handle(HttpRequest request, StringValidationException exception) {
+    return HttpResponse.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+  }
+}
