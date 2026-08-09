9b5b2c04a9d4666ee4e847d878c2099add9fb9b3
No1Evil <notevil036@gmail.com>
2026-08-09 11:46:02 +0300

    fix(backend): data insert column name fixes

--- appointments-service/src/main/resources/changelog/practitioner/data/01-practitioners_data.sql
+++ appointments-service/src/main/resources/changelog/practitioner/data/01-practitioners_data.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 --changeset f.tsumakov@gmail.com:01-practitioners_data.sql
 
-insert into practitioners(id, first_name, last_name, service_name)
+insert into practitioners(id, first_name, last_name, service_code)
 values ('019fdd5a-8c0e-75d4-987c-2064ed5ce5df', 'Practitioner1', 'First', 'dental');
 
-insert into practitioners(id, first_name, last_name, service_name)
-values ('019fdd5a-8c0e-75d4-987c-2482a0b4b72d', 'Practitioner2', 'Second', 'health');
+insert into practitioners(id, first_name, last_name, service_code)
+values ('019fdd5a-8c0e-75d4-987c-2482a0b4b72d', 'Practitioner2', 'Second', 'gp');
 
-insert into practitioners(id, first_name, last_name, service_name)
+insert into practitioners(id, first_name, last_name, service_code)
 values ('019fdd5a-8c0e-75d4-987c-2a0e272c914d', 'Practitioner3', 'Third', 'mental-health');
