1614502a56544b9ca492db74916802e2274b1b28
No1Evil <notevil036@gmail.com>
2026-08-09 18:51:01 +0300

    chore(frontend): configure routes

--- appointments-web/src/app/app.routes.ts
+++ appointments-web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppointmentComponent } from './appointments/list/appointment.component';
+import { SlotListComponent } from './slots/list/slot-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'slots' },
+  { path: 'slots', component: SlotListComponent },
+  { path: 'appointments', component: AppointmentComponent }
+];
