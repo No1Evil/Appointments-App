2fc412329be3389f13fa60d62223f4c841e8a575
No1Evil <notevil036@gmail.com>
2026-08-10 23:43:02 +0300

    fix(backend): SlotRepository use prepared statement to get generated key from entity

--- appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotRepository.java
+++ appointments-service/src/main/java/com/tsumakov/appointments/slot/SlotRepository.java
@@ -2,6 +2,7 @@ package dev.tsumakov.appointments.slot;
 
 import dev.tsumakov.appointments.common.repository.CrudRepository;
 import jakarta.annotation.Nonnull;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -49,12 +51,19 @@ public final class SlotRepository implements CrudRepository<Slot, Long> {
           end_time
         ) values (?, ?, ?, ?)
         """;
-    GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-    jdbcTemplate.update(sql, keyHolder,
-        entity.getStatusName(), entity.getService().getCode(),
-        entity.getStartTime(), entity.getEndTime());
+    KeyHolder keyHolder = new GeneratedKeyHolder();
 
-    return keyHolder.getKey() != null ? keyHolder.getKey().longValue() : null;
+    jdbcTemplate.update(connection -> {
+      PreparedStatement ps = connection.prepareStatement(sql, new String[] {"id"}); // Or Statement.RETURN_GENERATED_KEYS
+      ps.setString(1, entity.getStatusName());
+      ps.setString(2, entity.getService().getCode());
+      ps.setObject(3, entity.getStartTime());
+      ps.setObject(4, entity.getEndTime());
+      return ps;
+    }, keyHolder);
+
+    Number key = keyHolder.getKey();
+    return key != null ? key.longValue() : null;
   }
 
   @Override
@@ -79,7 +88,7 @@ public final class SlotRepository implements CrudRepository<Slot, Long> {
     return rowsUpdated > 0;
   }
 
-  public List<Slot> listByFilter(SlotParams params) {
+  public List<Slot> listByFilter(@Nonnull SlotParams params) {
     StringBuilder sql = new StringBuilder("select * from slots where 1=1");
     List<Object> args = new ArrayList<>();
 
